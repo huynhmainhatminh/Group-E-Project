8da68e3a739731c12480f50e6c9ec75a206292fa
ThanhXCL <153916975+ThanhXCL@users.noreply.github.com>
2024-11-03 20:45:42 +0700

    Add files via upload

---
+++ APITVCAKOI/TVCaKoi.BLL/FengShuiSvc.cs
@@ -0,0 +1,31 @@
+﻿using QLBH.DAL;
+using TVcaKoi.Common.BLL;
+using TVcaKoi.Common.Rsp;
+using TVCaKoi.Common.Res;
+using TVCaKoi.DAL;
+using TVCaKoi.DAL.Models;
+
+namespace TVCaKoi.BLL
+{
+    public class FengShuiSvc: GenericSvc<FengShuiRep, QlParameter>
+    {
+        private FengShuiRep fengshuiRep;
+        public FengShuiSvc()
+        {
+            fengshuiRep = new FengShuiRep();
+        }
+        public override SingleRsp Read(int id)
+        {
+            var res = new SingleRsp();
+            res.Data = _rep.Read(id);
+            return res;
+        }
+
+        public override SingleRsp Read(string key)
+        {
+            var res = new SingleRsp();
+            res.Data = _rep.Read(key);
+            return res;
+        }
+    }
+}
