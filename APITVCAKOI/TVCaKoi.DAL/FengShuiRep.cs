664923598ac80629b2bde2eac428ea698ccabc68
ThanhXCL <153916975+ThanhXCL@users.noreply.github.com>
2024-11-03 20:43:49 +0700

    Add files via upload

---
+++ APITVCAKOI/TVCaKoi.DAL/FengShuiRep.cs
@@ -0,0 +1,24 @@
+﻿using TVcaKoi.Common.DAL;
+using TVcaKoi.Common.Rsp;
+using TVCaKoi.Common.Res;
+using TVCaKoi.DAL.Models;
+
+namespace TVCaKoi.DAL
+{
+    public class FengShuiRep: GenericRep<ApptvcakoiContext, QlParameter>
+    {
+        public FengShuiRep() { }
+
+        public override QlParameter Read(int id)
+        {
+            var res = All.FirstOrDefault(c => c.IdqlParameter == id);
+            return res;
+        }
+
+        public override QlParameter Read(string key)
+        {
+            var res = All.FirstOrDefault(c => c.NameDestiny == key);
+            return res;
+        }
+    }
+}
