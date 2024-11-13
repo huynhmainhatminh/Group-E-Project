80883c8ac615947f245f637c614ed940c80ec258
Huỳnh Mai Nhật Minh <156114356+huynhmainhatminh@users.noreply.github.com>
2024-11-14 02:40:54 +0700

    Add files via upload

---
+++ TVCaKoiPhongThuy/TVCaKoi.WebApp/Models/RspBase.cs
@@ -0,0 +1,12 @@
+﻿namespace TVCaKoi.WebApp.Models
+{
+    public class RspBase
+    {
+        public object Data { get; set; }
+        public bool Success { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public string Variant { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ TVCaKoiPhongThuy/TVCaKoi.WebApp/Models/Rsp_FengShui.cs
@@ -0,0 +1,27 @@
+﻿namespace TVCaKoi.WebApp.Models
+{
+    public class PhongThuyResponse
+    {
+        public DataPhongThuy data { get; set; }
+        public bool success { get; set; }
+        public string message { get; set; }
+    }
+
+    public class DataPhongThuy
+    {
+        public int IdqlParameter { get; set; }
+
+        public string NameDestiny { get; set; }
+
+        public string NumberFish { get; set; }
+
+        public string Direction { get; set; }
+
+        public string? Color { get; set; }
+
+        public string? ColorFit { get; set; }
+
+        public string? ColorNotFit { get; set; }
+    }
+
+}
